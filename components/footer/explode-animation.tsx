1f101ab299edbdc786dfa457de6f5521a380a003
characterMi <abol1385fx@gmail.com>
2025-05-27 19:02:38 +0330

    adding a random color to explode animation

--- components/footer/explode-animation.tsx
+++ components/footer/explode-animation.tsx
@@ -27,16 +27,15 @@ const ExplodeAnimation = ({ text, style }: Props) => {
   function animate(): TargetAndTransition {
     if (!isAnimating) return {};
 
-    const randomRotation = (Math.random() - 0.5) * 100;
-    const randomX = (Math.random() - 0.5) * 100;
-    const randomY = (Math.random() - 0.5) * 100;
-    const randomScale = Math.random() + 0.5;
+    const colors = ["#FFFFFF", "#56ccf2", "#b6edff", "#32a2c7"];
 
     return {
-      rotate: [0, randomRotation, 0],
-      x: [0, randomX, 0],
-      y: [0, randomY, 0],
-      scale: [1, randomScale, 1],
+      // 0%, 100% { rotate: 0 } 50% { rotate: -58 }
+      rotate: [0, (Math.random() - 0.5) * 100, 0],
+      x: [0, (Math.random() - 0.5) * 100, 0],
+      y: [0, (Math.random() - 0.5) * 100, 0],
+      color: [colors[0], colors[~~(Math.random() * colors.length)], colors[0]],
+      scale: [1, (Math.random() + 0.5).toFixed(2), 1],
       transition: {
         ease: ease2,
         duration: 2,
@@ -47,20 +46,25 @@ const ExplodeAnimation = ({ text, style }: Props) => {
 
   return (
     <p
-      className="cursor-pointer overflow-visible inline-flex underline"
+      className="cursor-pointer overflow-visible inline-flex"
       onClick={handleClick}
       style={style}
     >
-      {text.split("").map((letter, i) => (
-        <motion.span
-          key={i}
-          initial={initial}
-          animate={animate()}
-          onAnimationComplete={() => setIsAnimating(false)}
-        >
-          {letter}
-        </motion.span>
-      ))}
+      {text.split("").map((letter, i) => {
+        if (letter === " ") return <span key={i}>&nbsp;</span>;
+
+        return (
+          <motion.span
+            key={i}
+            initial={initial}
+            animate={animate()}
+            onAnimationComplete={() => setIsAnimating(false)}
+            className="underline"
+          >
+            {letter}
+          </motion.span>
+        );
+      })}
     </p>
   );
 };
